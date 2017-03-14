374bbcac7c4379879244f11fe4b839c9b097c0d7
Joao Pedro Portela <jportela.gomez@gmail.com>
2017-03-14 03:15:46 -0300

    missing content on iteritems()

--- flask_cloudant/__init__.py
+++ flask_cloudant/__init__.py
@@ -145,7 +145,7 @@ class FlaskCloudantDocument(object):
             return dict(self.document)
         else:
             assert type(content) is dict
-            for key, value in iteritems():
+            for key, value in content.iteritems():
                 self.document.field_set(self.document, key, value)
 
     def exists(self):
