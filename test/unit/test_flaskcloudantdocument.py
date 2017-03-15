1def7dc442f9a8adab9ef0b439157509a43ad650
Joao Pedro Portela <jportela.gomez@gmail.com>
2017-03-14 23:49:42 -0300

    preparing tests for FlaskCloudantDocument

---
+++ test/unit/test_flaskcloudantdocument.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+'''
+Unit tests.
+'''
+
+from flask_cloudant import FlaskCloudantDocument
+from flask_cloudant.error import FlaskCloudantException
+import flask
+import pytest
+
+
+@pytest.fixture
+def app():
+    pass
+
+
+def test_content(app):
+    pass
+
+
+def test_exists(app):
+    pass
+
+
+def save(app):
+    pass
+
+
+def delete(app):
+    pass
